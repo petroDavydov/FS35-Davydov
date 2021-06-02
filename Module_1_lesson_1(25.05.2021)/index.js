ff7462b4c053aacd51163c0160aa22e95bebf663
petroDavydov <petrodavydov@gmail.com>
2021-06-02 15:32:23 +0300

    add folders

---
+++ Module_1_lesson_1(25.05.2021)/index.js
@@ -0,0 +1,165 @@
+
+// function sum() {
+//     return 2 + 3;
+// }
+
+// let result = sum();
+
+// console.log(result)
+
+
+
+// Ошибки в консоли
+
+// const name;
+
+// console.log(age);
+// let age;
+
+// console.log('HELLO WORLSd')
+
+// Определение типа значения, оператор typeof
+// Валидация форм
+
+// const productName = 'Mango';
+// const pricePerItem = 150;
+// const isOnSale = false;
+// const error = null;
+// let quantity;
+
+// console.log(typeof productName);
+// console.log(typeof pricePerItem);
+// console.log(typeof isOnSale);
+// console.log(typeof error);
+// console.log(typeof quantity);
+
+// Шаблонные строки
+// // Написать фразу с помощью шаблонных строк Здравствуйте, меня зовут А, мне Б лет и мне нравится Ц, где А, Б, Ц - переменные вставленные в строку.
+
+
+// const name = 'Манго';
+// const age = 5;
+// const hobby = 'плавать';
+// const message = `Здравствуйте, меня зовут ${name} мне ${age} лет и мне нравиться ${hobby}`;
+
+// console.log(message);
+
+// Prompt и confirm
+
+// Спросить у пользователя имя, почту,  телефон и показать в консоле фразу "Пользоваель под именем ____ использует ____ почту и ____ телелефон"
+// 1) Спросить имя юзера (prompt)
+// 2) Спросить почту (prompt)
+// 3) Спросить телефон (prompt)
+// 4) Составить финальную фразу (template string)
+
+// const userName = prompt('Enter your name');
+// const email = prompt('Enter your email');
+// const phoneNumber = prompt('Enter phon number');
+
+// const message = `Пользоваель под именем ${userName} использует ${email} почту и ${phoneNumber} телелефон`;
+// console.log(message);
+
+
+// let userName, email, phone;
+
+//  Базовые операторы +,-,*,/
+
+// Проверка сложения
+
+// let a = "Join ";
+// let b = "us";
+// console.log(a + b);
+// console.log(typeof a + b)
+
+// let a = 3;
+// let b = ' join';
+// console.log(a + b);
+
+// let a = true;
+// let b = " join";
+// console.log(a + b);
+
+// let a = true;
+// let b = 12;
+// console.log(a + b);
+
+// let a = true;
+// let b = true;
+// console.log(a + b);
+
+// let a = true;
+// let b = "false";
+// console.log(a + b);
+
+// let a = 12;
+// let b = null;
+// console.log(a + b);
+
+// let a = undefined;
+// let b = 10;
+// console.log(a + b);
+
+// let a = undefined;
+// let b = false;
+// console.log(a + b);
+
+// let a = 6;
+// let b = false;
+// let c = "12";
+// let d = 1;
+// let result = a + d + c + b;
+// console.log(result);
+// console.log(typeof result);
+
+
+// Обьект Math
+// // Определить площадь круга с радиусом 10см (ПR(2)). Math.pow(a, b) - возведение в степень
+
+// const rad = 10;
+// const PI = Math.PI;
+// const powRad = Math.pow(rad, 2);
+// const result = PI * powRad;
+// console.log('result', result)
+
+
+
+// const rad = 10;
+// const area = Math.PI * Math.pow(rad, 2);
+// console.log(area);
+
+
+
+//Операторы сравнения и приведение типов
+// // Каким будет результат этих выражений?
+
+// console.log(5 > 4);
+// console.log(10 >= "7"); 
+// console.log("2" > "12");
+// console.log("2" < "12");
+// console.log("4" == 4);
+// console.log("6" === 6);
+// console.log("false" === false);
+// console.log(1 == true);
+// console.log(1 === true);
+// console.log("0" == false);
+// console.log("0" === false);
+// console.log("Ёнанас" < "ёжик");
+// console.log("Ёнанас" === "ёжик");
+// console.log(undefined == null);
+// console.log(undefined === null);
+
+// Посчитать суму товаров в козине магазина
+// Нужно ввести цену 3-х товаров и посчитать их суму
+
+// const phonePrice = Number(prompt('Enter phone price')); // 10
+// const tvPrice = +prompt('Enter tv price'); // 12
+// const playStationPrice = Number(prompt('Enter ps price')) // 8
+
+// const result = phonePrice + tvPrice + playStationPrice;
+
+// console.log(result); // 30
+
+
+
+
+ 
